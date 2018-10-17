8bd1481eb8ac4807cd1d9c0d0cbe7a2749a7adb2
Mohammad Raunak <raunak@loyola.edu>
2018-10-16 22:52:40 -0400

    Update Card.java

--- app/src/main/java/sfpugh/loyola/edu/asgn01/Card.java
+++ app/src/main/java/sfpugh/loyola/edu/asgn01/Card.java
@@ -4,11 +4,13 @@ package sfpugh.loyola.edu.asgn01;
  * Card represents a single playing card. A playing card holds two values: the suit and the rank of
  * the card.
  * @author Sydney Pugh
- * @version 1.0
- * @since 2018-10-06
+ * @version 1.0 10/08/2018
+ *     -- since is used for representing since which release is this class added --
+ @since 2018-10-06
  */
 public class Card {
 
+    /** Write Javadoc description of your instance variables, e.g., The suit of the Card */
     private String suit;
     private String rank;
 
